e41c0191fcfc255c22419851ccf32c895e5db73e
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-14 23:16:19 +0300

    awdawd

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Document } from "mongoose";
 import { AxiosResponse } from "axios";
 import { CreateChatCompletionResponse } from "openai";
 
@@ -23,7 +24,7 @@ export type PromptName = keyof PromptMap;
 
 export type PromptGraph = { name: PromptName; level: number }[];
 
-export interface WhiteRequestForAccount {
+export interface WhiteRequestForAccount extends Document {
   email: string;
   key: string;
   idea?: string;
@@ -31,14 +32,14 @@ export interface WhiteRequestForAccount {
   updatedAt?: Date;
 }
 
-export interface WhiteRequestForPassChange {
+export interface WhiteRequestForPassChange extends Document {
   email: string;
   key: string;
   createdAt?: Date;
   updatedAt?: Date;
 }
 
-export interface WhiteUser {
+export interface WhiteUser extends Document {
   email: string;
   passwordHash: string;
   name: string;
@@ -51,6 +52,6 @@ export type OpenAIPromise = Promise<
   AxiosResponse<CreateChatCompletionResponse, any>
 >;
 
-export interface WhiteOpenAIPromise {
+export interface WhiteOpenAIPromise extends Document {
   stringifiedOpenAIPromise: string;
 }
