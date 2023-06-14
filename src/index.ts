254da813786d2c0bfb08121817a71a450080dc21
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-14 22:35:04 +0300

    asd

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,27 @@ export type Prompt = PromptPart[];
 export type PromptMap = Record<string, Prompt>;
 
 export type PromptGraph = { name: string; level: number }[];
+
+export interface WhiteRequestForAccount {
+  email: string;
+  key: string;
+  idea?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface WhiteRequestForPassChange {
+  email: string;
+  key: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface WhiteUser {
+  email: string;
+  passwordHash: string;
+  name: string;
+  subscription: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
