550d4fd851e44b60cf5e4a472e257340ee4fc5da
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-17 11:56:06 +0300

    Create api.ts

---
+++ src/api.ts
@@ -0,0 +1,10 @@
+export namespace API {
+  export namespace Accounts {
+    export namespace Accounts {}
+    export namespace Accounts {}
+    export namespace Accounts {}
+  }
+  export namespace Auth {}
+  export namespace Data {}
+  export namespace Website {}
+}
