dcf39b664794532158da624462fdd9522d4b4f73
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-29 14:01:19 +0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -142,7 +142,7 @@ export namespace API {
     export namespace RunAndGetPromptResult {
       export interface Req {
         ideaId: string;
-        promptName: PromptName;
+        promptNames: PromptName;
         feedback?: string;
       }
       export interface Res {
