7f45df0eb21625e48eafc2610c0b8912b22c17bb
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-02 16:58:26 +0300

    asd

--- src/oc-models/index.ts
+++ src/oc-models/index.ts
@@ -1,7 +1,15 @@
 import { Document } from "mongoose";
 
 export namespace OCModels {
-  export interface WhiteToken extends Document {
+  export interface RestRequest extends Document {
     stringified: string;
+  }
+
+  export interface SigninReq extends Document {
+    stringifiedReq: string;
+    successfull: boolean;
+    userEmail: string;
+    time: Date;
+    reason?: string;
   }
 }
